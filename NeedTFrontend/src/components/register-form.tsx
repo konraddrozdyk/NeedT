3d2863c8ff3c57ed156689fa08eadc021f9b6c6b
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-17 17:14:49 +0100

    Layout and style changes to register and login components

---
+++ NeedTFrontend/src/components/register-form.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Registrera</CardTitle>
+          <CardDescription>
+            Fyll i dina registeringsuppgifter för att gå vidare
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Användarnamn</Label>
+                <Input
+                  id="username"
+                  type="username"
+                  placeholder="Användarnamn"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Lösenord</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Gå vidare
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              <a href="/" className="underline underline-offset-4">
+                Gå tillbaka
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- NeedTFrontend/src/routes/register.tsx
+++ NeedTFrontend/src/routes/register.tsx
@@ -1,3 +1,4 @@
+import { RegisterForm } from "@/components/register-form";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/register")({
@@ -6,31 +7,8 @@ export const Route = createFileRoute("/register")({
 
 function RegisterComponent() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="bg-white p-6 rounded-md shadow-lg w-full max-w-xs">
-        <h2 className="text-xl font-semibold">Registrera dig</h2>
-        <form className="mt-4">
-          <input
-            type="text"
-            placeholder="Användarnamn"
-            className="border border-gray-300 p-2 rounded w-full"
-          />
-          <div className="mt-4">
-            <button
-              type="submit"
-              className="bg-black text-white px-4 py-2 rounded w-full"
-            >
-              Registrera
-            </button>
-          </div>
-        </form>
-        <p className="mt-4 text-center text-sm">
-          Har du redan ett konto?{" "}
-          <a href="/" className="text-blue-500 hover:underline">
-            Logga in här
-          </a>
-        </p>
-      </div>
+    <div className="flex items-center justify-center h-screen">
+      <RegisterForm />
     </div>
   );
 }
