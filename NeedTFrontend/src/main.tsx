96d7bb9f62abd632039e049ecc1188dce58a8ca0
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-17 15:14:44 +0100

    Task: Set up application routes progression

--- NeedTFrontend/src/main.tsx
+++ NeedTFrontend/src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
