80ae8348b0f34411924f8b73a96c946087fa2cfe
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-18 22:10:13 +0100

    Implements controller for all job-related services

--- NeedTBackend/Controllers/JobsControllers.cs
+++ NeedTBackend/Controllers/JobsControllers.cs
@@ -46,4 +46,70 @@ public class JobsController : ControllerBase
             return NotFound(e.Message);
         }
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<JobDto>>> GetJobsAsync()
+    {
+        var jobs = await _jobService.GetJobsAsync();
+        return Ok(jobs);
+    }
+
+    [HttpGet("pending")]
+    public async Task<ActionResult<IEnumerable<JobDto>>> GetPendingJobsAsync()
+    {
+        var jobs = await _jobService.GetPendingJobsAsync();
+        return Ok(jobs);
+    }
+
+    [HttpGet("accepted")]
+    public async Task<ActionResult<IEnumerable<JobDto>>> GetAcceptedJobsAsync()
+    {
+        var jobs = await _jobService.GetAcceptedJobsAsync();
+        return Ok(jobs);
+    }
+
+    [HttpGet("completed")]
+    public async Task<ActionResult<IEnumerable<JobDto>>> GetCompletedJobsAsync()
+    {
+        var jobs = await _jobService.GetCompletedJobsAsync();
+        return Ok(jobs);
+    }
+
+    [HttpPatch("{id}/accept")]
+    public async Task<ActionResult<JobDto>> AcceptJobAsync(int id)
+    {
+        try
+        {
+            var job = await _jobService.AcceptJobAsync(id);
+            return Ok(job);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPatch("{id}/complete")]
+    public async Task<ActionResult<JobDto>> CompleteJobAsync(int id)
+    {
+        try
+        {
+            var job = await _jobService.CompleteJobAsync(id);
+            return Ok(job);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    
 }
