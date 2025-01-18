db672c43f71c6f53f46904e1762e7431e42c82f3
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-18 17:47:20 +0100

    Added conditional rendering to orderer-dashboard that prohibits not logged in users from filling order forms

--- NeedTFrontend/src/routes/orderer-dashboard.tsx
+++ NeedTFrontend/src/routes/orderer-dashboard.tsx
@@ -1,11 +1,18 @@
 import OrderForm from "@/components/orderer-form";
 import { createFileRoute } from "@tanstack/react-router";
+import { useUser } from "../context/user-context";
 
 export const Route = createFileRoute("/orderer-dashboard")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { user } = useUser();
+
+  if (!user || user.role !== "orderer") {
+    return <div>Not authorized</div>;
+  }
+
   return (
     <div>
       <OrderForm />
